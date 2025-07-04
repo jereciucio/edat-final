2569e5b660d01b8a5a83627bf3baa4c06095edf1
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-07-04 18:23:31 -0300

    NodoAdy: Constructor

--- lib/src/main/java/grafos/dinamicas/NodoAdy.java
+++ lib/src/main/java/grafos/dinamicas/NodoAdy.java
@@ -3,4 +3,11 @@ public class NodoAdy {
   NodoVert vertice;
   NodoAdy sigAdyacente;
   Object etiqueta;
+  
+  public NodoAdy(NodoVert elVertice, Object laEtiqueta) {
+    // Recibe como primer parámetro el vértice hacia el que se dirige el arco.
+    this.vertice = elVertice;
+    this.sigAdyacente = null;
+    this.etiqueta = laEtiqueta;
+  }
 }
