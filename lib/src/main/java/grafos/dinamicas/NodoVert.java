2667aefd4ff689cbfcfce9557f8190457e3adc83
Joaquin Exequiel Ortega <ortegajoaquinexequiel@gmail.com>
2025-07-05 03:13:06 -0300

    NodoVert: Constructor

--- lib/src/main/java/grafos/dinamicas/NodoVert.java
+++ lib/src/main/java/grafos/dinamicas/NodoVert.java
@@ -3,4 +3,10 @@ public class NodoVert{
   private Object elem;
   private NodoVert sigVertice;
   private NodoAdy primerAdy;
+
+   public NodoVert(Object unElemento, NodoVert unSigVertice, NodoAdy unPrimerAdy) {
+        this.elem = unElemento;
+        this.sigVertice = unSigVertice;
+        this.primerAdy = unPrimerAdy;
+    }
 }
