b8150acfd7f5d7a12511b2a0ff959d436ac4a6b7
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-07-15 18:39:51 -0300

    DatosTuberia: Clase base y atributos

---
+++ lib/src/main/java/propiasDelDominio/DatosTuberia.java
@@ -0,0 +1,8 @@
+package propiasDelDominio;
+public class DatosTuberia {
+  private double caudalMinimoM3;
+  private double caudalMaximoM3;
+  private double diametroMM;
+  private String nomenclatura;
+  private String estado;
+}
