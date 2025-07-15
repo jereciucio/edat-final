6d3afb638f4a0341bb24c5f21c7dba1f313b53e6
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-07-15 19:41:53 -0300

    DominioTuberia: Constructor

--- lib/src/main/java/propiasDelDominio/DominioTuberia.java
+++ lib/src/main/java/propiasDelDominio/DominioTuberia.java
@@ -2,4 +2,9 @@ package propiasDelDominio;
 public class DominioTuberia {
   String ciudadOrigen;
   String ciudadDestino;
+
+  public DominioTuberia(String nomCiudadOrigen, String nomCiudadDestino) {
+    this.ciudadOrigen = nomCiudadOrigen;
+    this.ciudadDestino = nomCiudadDestino;
+  }
 }
