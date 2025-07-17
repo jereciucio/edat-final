773c3efbadca68456a2cd02f4180e07ed753eec5
Joaquin Exequiel Ortega <ortegajoaquinexequiel@gmail.com>
2025-07-17 16:57:51 -0300

    Anio: getValor(int)

--- lib/src/main/java/propiasDelDominio/Anio.java
+++ lib/src/main/java/propiasDelDominio/Anio.java
@@ -3,9 +3,18 @@ package propiasDelDominio;
 public class Anio {
   private int numeroAnio;
   private int[] meses;
-  
+
   public Anio(int unNumeroAnio) {
     this.numeroAnio = unNumeroAnio;
     this.meses = new int[12];
+  }
+  public Object getValor(int unMes) {
+    Object valor;
+    if (unMes >= 1 && unMes <= 12) {
+      valor = this.meses[unMes - 1];
+    } else {
+      valor = null;
+    }
+    return valor;
   }
 }
