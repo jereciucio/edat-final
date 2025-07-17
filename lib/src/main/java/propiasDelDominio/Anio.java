302ec1fdfdf056072bf1fe2b7febaaee333b8687
Joaquin Exequiel Ortega <ortegajoaquinexequiel@gmail.com>
2025-07-17 16:56:43 -0300

    Anio: Constructor

--- lib/src/main/java/propiasDelDominio/Anio.java
+++ lib/src/main/java/propiasDelDominio/Anio.java
@@ -3,4 +3,9 @@ package propiasDelDominio;
 public class Anio {
   private int numeroAnio;
   private int[] meses;
+  
+  public Anio(int unNumeroAnio) {
+    this.numeroAnio = unNumeroAnio;
+    this.meses = new int[12];
+  }
 }
