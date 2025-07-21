51610f4e3e0170b9616311b68a49c4ffc7850177
Roger743 <rogerweinberger5@gmail.com>
2025-07-21 16:48:04 -0300

    Constructor de Ciudad

--- lib/src/main/java/propiasDelDominio/Ciudad.java
+++ lib/src/main/java/propiasDelDominio/Ciudad.java
@@ -8,4 +8,13 @@ public class Ciudad {
     private ArbolAVL calendarioHabitantes;
     private double superficie;
     private double consumoPerCapita;
+
+    public Ciudad(String nombre, String nomenclatura, ArbolAVL calendario, double superficie, double consumo){
+        this.nombre=nombre;
+        this.nomenclatura=nomenclatura;
+        this.calendarioHabitantes=calendario;
+        this.superficie=superficie;
+        this.consumoPerCapita=consumo;
+    }
+  
 }
