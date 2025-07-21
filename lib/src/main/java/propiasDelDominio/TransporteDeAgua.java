f9cb55594e46484716e4c0454bc44f86b8b9497d
Roger743 <rogerweinberger5@gmail.com>
2025-07-21 17:17:14 -0300

    Atributos de TransporteDeAgua

--- lib/src/main/java/propiasDelDominio/TransporteDeAgua.java
+++ lib/src/main/java/propiasDelDominio/TransporteDeAgua.java
@@ -1,1 +1,11 @@
+package propiasDelDominio;
 
+import java.util.HashMap;
+import conjuntistas.ArbolAVL;
+import grafos.Grafo;
+
+public class TransporteDeAgua {
+  private ArbolAVL arbolCiudades;
+  private Grafo grafoTuberias;
+  private HashMap mapeoTuberias;
+}
