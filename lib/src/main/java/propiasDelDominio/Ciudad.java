5cdfbc5507efd16e54ba662d8c4aa8e335c7cfed
Roger743 <rogerweinberger5@gmail.com>
2025-07-21 16:46:24 -0300

    atributos de Ciudad

--- lib/src/main/java/propiasDelDominio/Ciudad.java
+++ lib/src/main/java/propiasDelDominio/Ciudad.java
@@ -1,1 +1,11 @@
+package propiasDelDominio;
 
+import conjuntistas.ArbolAVL;
+
+public class Ciudad {
+    private String nombre;
+    private String nomenclatura;
+    private ArbolAVL calendarioHabitantes;
+    private double superficie;
+    private double consumoPerCapita;
+}
