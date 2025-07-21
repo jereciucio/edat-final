939517b67a74949f6b42373a85d363de01266674
Roger743 <rogerweinberger5@gmail.com>
2025-07-21 16:51:02 -0300

    getNombre() de Ciudad

--- lib/src/main/java/propiasDelDominio/Ciudad.java
+++ lib/src/main/java/propiasDelDominio/Ciudad.java
@@ -3,18 +3,22 @@ package propiasDelDominio;
 import conjuntistas.ArbolAVL;
 
 public class Ciudad {
-    private String nombre;
-    private String nomenclatura;
-    private ArbolAVL calendarioHabitantes;
-    private double superficie;
-    private double consumoPerCapita;
+  private String nombre;
+  private String nomenclatura;
+  private ArbolAVL calendarioHabitantes;
+  private double superficie;
+  private double consumoPerCapita;
 
-    public Ciudad(String nombre, String nomenclatura, ArbolAVL calendario, double superficie, double consumo){
-        this.nombre=nombre;
-        this.nomenclatura=nomenclatura;
-        this.calendarioHabitantes=calendario;
-        this.superficie=superficie;
-        this.consumoPerCapita=consumo;
-    }
+  public Ciudad(String nombre, String nomenclatura, ArbolAVL calendario, double superficie, double consumo){
+    this.nombre=nombre;
+    this.nomenclatura=nomenclatura;
+    this.calendarioHabitantes=calendario;
+    this.superficie=superficie;
+    this.consumoPerCapita=consumo;
+  }
+
+  public String getNombre(){
+    return this.nombre;
+  }
   
 }
