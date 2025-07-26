4a5a43473dea61dc015629cff0e1aabb28a97e55
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-07-25 22:41:24 -0300

    ArbolAVLMapeo: Añadir existeClave()

--- lib/src/main/java/propositoEspecifico/ArbolAVLMapeo.java
+++ lib/src/main/java/propositoEspecifico/ArbolAVLMapeo.java
@@ -6,4 +6,25 @@ public class ArbolAVLMapeo {
   public ArbolAVLMapeo() {
     this.raiz = null;
   }
+
+  public boolean existeClave(Comparable laClave) {
+    return existeClaveAux(this.raiz, laClave);
+  }
+
+  private boolean existeClaveAux(NodoAVLMapeo nodoActual, Comparable laClave) {
+    int comparacion = 0;
+    boolean exito = false;
+    if (nodoActual != null) {
+      comparacion = laClave.compareTo(nodoActual.getClave());
+
+      if (comparacion < 0) {
+        exito = existeClaveAux(nodoActual.getIzquierdo(), laClave);
+      } else if (comparacion > 0) {
+        exito = existeClaveAux(nodoActual.getDerecho(), laClave);
+      } else {
+        exito = true;
+      }
+    }
+    return exito;
+  }
 }
