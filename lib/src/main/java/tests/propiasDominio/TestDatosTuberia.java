c594ec6d61c406f5549c186cd9a3bc4ad1bc3d9b
roger743 <roger.weimberger@est.fi.uncoma.edu.ar>
2025-08-05 22:32:19 -0300

    TestDatosTuberia

---
+++ lib/src/main/java/tests/propiasDominio/TestDatosTuberia.java
@@ -0,0 +1,15 @@
+package tests.propiasDominio;
+
+import propiasDelDominio.DatosTuberia;
+
+public class TestDatosTuberia {
+  public static void main(String[] args) {
+    DatosTuberia datos=new DatosTuberia("NE3001", "CU3002", 200, 300, 20, "ACTIVA");
+    System.out.println(datos.getNomenclatura());
+    System.out.println(datos.getCaudalMinimo());
+    System.out.println(datos.getCaudalMaximo());
+    System.out.println(datos.getDiametro());
+    System.out.println(datos.getEstado());
+    System.out.println(datos.toString());
+  }
+}
