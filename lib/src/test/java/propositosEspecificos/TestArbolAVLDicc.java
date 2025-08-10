3219e0b5799c35cb3d91efbeab8f110919c74b21
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-08-10 14:50:24 -0300

    TestArbolAVLDicc: Añadir testInsertarRaiz()

--- lib/src/test/java/propositosEspecificos/TestArbolAVLDicc.java
+++ lib/src/test/java/propositosEspecificos/TestArbolAVLDicc.java
@@ -6,4 +6,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArbolAVLDicc {
+  @Test
+  public void testInsertarRaiz() {
+    ArbolAVLDicc tree = new ArbolAVLDicc();
+    boolean insertar = tree.insertar(1, 1);
+    String expectedTreeString = "Raiz: 1";
+    expectedTreeString += "\n1 (alt: 0)";
+    expectedTreeString += "\n    HI: -";
+    expectedTreeString += "\n    HD: -";
+    String treeString = tree.toString();
+    System.err.print(treeString);
+
+    assertEquals(expectedTreeString, treeString);
+    assertEquals(insertar, true);
+  }
 }
