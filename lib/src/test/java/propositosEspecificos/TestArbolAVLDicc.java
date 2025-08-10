d71c44a795105bc80ceeed690ac19773dc21a7b8
Jeremías Agustín Martinez Ciucio <jeremiasmartinez.ciucio@gmail.com>
2025-08-10 15:02:11 -0300

    TestArbolAVLDicc: Añadir testInsertarYaPresente()

--- lib/src/test/java/propositosEspecificos/TestArbolAVLDicc.java
+++ lib/src/test/java/propositosEspecificos/TestArbolAVLDicc.java
@@ -15,9 +15,46 @@ public class TestArbolAVLDicc {
     expectedTreeString += "\n    HI: -";
     expectedTreeString += "\n    HD: -";
     String treeString = tree.toString();
-    System.err.print(treeString);
+    System.err.println(treeString);
 
     assertEquals(expectedTreeString, treeString);
     assertEquals(insertar, true);
+  }
+
+  @Test
+  public void testInsertarYaPresente() {
+    ArbolAVLDicc tree = new ArbolAVLDicc();
+    String expectedTreeString = "Raiz: 9";
+    expectedTreeString += "\n9 (alt: 2)";
+    expectedTreeString += "\n    HI: 7";
+    expectedTreeString += "\n    HD: 10";
+    expectedTreeString += "\n7 (alt: 1)";
+    expectedTreeString += "\n    HI: -";
+    expectedTreeString += "\n    HD: 8";
+    expectedTreeString += "\n8 (alt: 0)";
+    expectedTreeString += "\n    HI: -";
+    expectedTreeString += "\n    HD: -";
+    expectedTreeString += "\n10 (alt: 0)";
+    expectedTreeString += "\n    HI: -";
+    expectedTreeString += "\n    HD: -";
+
+    tree.insertar(10, 10);
+    tree.insertar(7, 7);
+    tree.insertar(9, 9);
+    tree.insertar(8, 8);
+
+    boolean insertar1 = tree.insertar(9, 9);
+    boolean insertar2 = tree.insertar(7, 7);
+    boolean insertar3 = tree.insertar(8, 8);
+    boolean insertar4 = tree.insertar(10, 10);
+
+    String treeString = tree.toString();
+    System.err.println(treeString);
+
+    assertEquals(insertar1, false);
+    assertEquals(insertar2, false);
+    assertEquals(insertar3, false);
+    assertEquals(insertar4, false);
+    assertEquals(expectedTreeString, treeString);
   }
 }
